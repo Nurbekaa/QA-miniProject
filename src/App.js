d1bdf2ad391176642aa27048a57fffd6c3bae9a8
Nurbekaa <nurbek_ybyray@mail.ru>
2024-04-01 22:18:30 +0500

    remove unnecessary item
    remove unnecessary item

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
-import { useState } from 'react';
 import './App.css';
-// import {Count} from "./counter"
 import { Acc } from "./Accardion"
 
 
-function App(){
-
-  const [hide, setHide] = useState(true);
-  function showHide(){
-    setHide(!hide);
-  }
-  return(
-    <div className='App'>
-      <button onClick={showHide}>{hide ? "Hide" : "Show"}</button>
-      {hide && <h1>Hello World!!!</h1>}
-    </div>
-  )
-}
 export default Acc;
 
